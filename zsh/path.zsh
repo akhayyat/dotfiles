e4c35fa7f2ff08df6aaa21808692eafafcde6070
Ahmad Khayyat <akhayyat@gmail.com>
2019-11-10 21:55:09 +0300

    zsh: split programs into path and programs; use lsd if installed

---
+++ zsh/path.zsh
@@ -0,0 +1,24 @@
+#
+# PATH
+#
+
+# Correct commands
+setopt correct
+
+# pip
+export PATH=~/.local/bin:$PATH
+
+# rvm
+if [ -x $HOME/.rvm/scripts/rvm ]; then
+    source $HOME/.rvm/scripts/rvm
+fi
+export PATH=$HOME/.rvm/bin:$PATH
+
+# grails
+if [ -x $HOME/local/software/grails/bin/grails ]; then
+   export GRAILS_HOME=$HOME/local/software/grails
+   export PATH=$PATH:$GRAILS_HOME/bin
+fi
+
+# rust
+export PATH=$HOME/.cargo/bin:$PATH
--- zsh/programs.zsh
+++ zsh/programs.zsh
@@ -2,48 +2,8 @@
 # Program-specific settings
 #
 
-# Correct commands
-setopt correct
-
-# ls
-eval "$(dircolors)"
-alias ls="ls --color=auto --group-directories-first"
-alias l=ls
-alias lh="ls -lh"
-alias ll="ls -l"
-alias la="ls -A"
-
-# grep
-alias grep='grep --color=auto'
-
-# less
-export LESS='-i -M -R -w -z-2'
-
-# Set the less input preprocessor
-# pless (pretty less) is less with syntax hilighting
-# requies the source-highlight package
-if (( $+commands[lesspipe] )); then
-    export LESSOPEN='| /usr/bin/env lesspipe %s 2>&-'
-    alias pless='LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s" less '
-fi
-
 # virtualenvwrapper
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-fi
-
-# pip
-export PATH=~/.local/bin:$PATH
-
-# rvm
-if [ -x $HOME/.rvm/scripts/rvm ]; then
-    source $HOME/.rvm/scripts/rvm
-fi
-export PATH=$HOME/.rvm/bin:$PATH
-
-# grails
-if [ -x $HOME/local/software/grails/bin/grails ]; then
-   export GRAILS_HOME=$HOME/local/software/grails
-   export PATH=$PATH:$GRAILS_HOME/bin
 if [ -r /usr/share/bash-completion/completions/virtualenvwrapper ]; then
     source /usr/share/bash-completion/completions/virtualenvwrapper
 fi
@@ -77,3 +37,32 @@ alias -g G="| grep -i"
 alias -g L="| less -R"
 alias -g C="| wc -l"
 alias -g T="| tail"
+
+# ls
+eval "$(dircolors)"
+# use lsd if installed
+if [ -x "$(which lsd)" ]; then
+    alias ls="lsd --group-dirs first"
+    alias lt="ls --tree"
+else
+    alias ls="ls --color=auto --group-directories-first"
+fi
+alias l=ls
+alias lh="ls -lh"
+alias ll="ls -l"
+alias la="ls -A"
+alias lla="ls -lA"
+
+# grep
+alias grep='grep --color=auto'
+
+# less
+export LESS='-i -M -R -w -z-2'
+
+# Set the less input preprocessor
+# pless (pretty less) is less with syntax hilighting
+# requies the source-highlight package
+if (( $+commands[lesspipe] )); then
+    export LESSOPEN='| /usr/bin/env lesspipe %s 2>&-'
+    alias pless='LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s" less '
+fi
